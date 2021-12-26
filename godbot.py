8288a45a03bfe3779b23c2638cd7998a8a84d2f0
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-12-26 19:01:51 +0530

    Create godbot.py

---
+++ godbot.py
@@ -0,0 +1,40 @@
+from telethon import TelegramClient as tg
+import os
+from telethon import events
+from telethon.tl.types import (ChannelParticipantsAdmins, ChatAdminRights,
+                               ChatBannedRights, MessageEntityMentionName,
+                               MessageMediaPhoto)
+
+from telethon.tl.functions.channels import (EditAdminRequest,
+                                            EditBannedRequest,
+                                            EditPhotoRequest)
+
+
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+god = tg("godboy", api_id=API_ID, api_hash=API_HASH).start(bot_token=BOT_TOKEN)
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+@god.on(events.NewMessage)
+async def _(e):
+  if e.is_channel:
+    await e.client(EditBannedRequest(e.chat_id, user.id, BANNED_RIGHTS))
+    await god.send_message(e.chat_id, "**SOMEONE IS SENDING MESSAGE VIA CHANNEL I BANNED THAT SHIT**")
+  else:
+    pass
+
+
+god.run_until_disconnected()
